c2c51f6a49e7086e5c519a2fc8cb87dc4cc59639
MartinaGangemi <martinagangemi94@gmail.com>
2022-03-25 16:10:53 +0100

    ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,15 @@ function user_name() {
     let x = document.getElementById("my_name").value;
     document.getElementById("show_name").innerHTML = x;
   }
+
+function myFunction() {
+    let my_age = document.getElementById("select_age");
+    document.getElementById("sconto").value = my_age;
+  }
+  
+//     function numero_km() {
+//     let x = document.getElementById("my_km").value;
+    
+//   }
+
+//   document.getElementById("show_name").innerHTML = x;
